220876558f664311c04ff594a5d5d9d45f6cc49e
JRBuizon <buizonjeremiah@gmail.com>
2024-08-18 11:38:55 +0800

    fix: images not loading

--- components/linkCard.tsx
+++ components/linkCard.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image, { StaticImageData } from "next/image";
 import clsx from "clsx";
 
-export default function LinkCard({ source, link, gif = false, className }: { source: StaticImageData | string, link: string, gif?: boolean, className?: string }) {
+export default function LinkCard({ source, link, gif = true, className }: { source: StaticImageData | string, link: string, gif?: boolean, className?: string }) {
     return (
         <div className={clsx("hover:scale-[102%] cursor-yapointer transition-all duration-300 ease-out relative w-full rounded-md overflow-clip h-[256px]", className)}>
             <Link target="_blank" href={link} className="outline-none cursor-yapointer" >
