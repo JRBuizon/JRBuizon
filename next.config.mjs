7206c5ebe23bbed43af220bfce22de44f3918a4e
JRBuizon <buizonjeremiah@gmail.com>
2024-11-14 19:18:32 +0800

    feat: notes on basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-    basePath: "",
+    basePath: "", //remember to add the check for production and the /JRBuizon basePath back if ever u decide to host on github again
     output: "export",
     images: {
         unoptimized: true,
