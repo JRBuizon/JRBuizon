0db9541deaf73f39249318a1e65bd42a43a4c291
JRBuizon <buizonjeremiah@gmail.com>
2024-11-14 19:15:25 +0800

    fix: added basePath back in but removed the check for prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+    basePath: "",
     output: "export",
     images: {
         unoptimized: true,
